75d10d89c8de158f384fb9cf76a3eac7013a5fbb
Gabriel Pedroso <lucred@protonmail.com>
2024-10-08 13:37:34 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <h1></h1>
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push("/introducao")
+  }, [])
 }
